0594d65fc391531ced0cb952c733c1347ed910ee
TanPhat23 <ptp112004@gmail.com>
2023-11-16 20:32:18 +0700

    remove getstaticparams

--- my-app/app/problems/[slug]/page.tsx
+++ my-app/app/problems/[slug]/page.tsx
@@ -6,12 +6,10 @@ import { notFound } from "next/navigation";
 import { firestore } from "@/lib/firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
-export async function generateStaticParams() {
-  const problems = await getDocs(collection(firestore, "problems"));
-  return problems.docs.map((d) => ({ slug: d.data()["id"] }));
-}
-
-
+// export async function generateStaticParams() {
+//   const problems = await getDocs(collection(firestore, "problems"));
+//   return problems.docs.map((d) => ({ slug: d.data()["id"] }));
+// }
 
 export default async function ProblemPage({
   params,
