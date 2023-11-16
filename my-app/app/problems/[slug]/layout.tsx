544f1b0087937f2eb4043df9b3ed013c107c4c92
TanPhat23 <ptp112004@gmail.com>
2023-11-16 17:50:39 +0700

    changed

--- my-app/app/problems/[slug]/layout.tsx
+++ my-app/app/problems/[slug]/layout.tsx
@@ -1,11 +1,3 @@
-import { firestore } from "@/lib/firebase/firebase";
-import { collection, getDocs } from "firebase/firestore";
-
-export async function generateStaticParams() {
-  const problems = await getDocs(collection(firestore, "problems"));
-  return problems.docs.map((d) => ({ slug: d.data()["id"] }));
-}
-
 const PageLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
--- my-app/app/problems/[slug]/page.tsx
+++ my-app/app/problems/[slug]/page.tsx
@@ -3,6 +3,13 @@ import Workspace from "@/components/problemComponents/Workspace";
 
 import { problems } from "@/lib/problems";
 import { notFound } from "next/navigation";
+import { firestore } from "@/lib/firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+export async function generateStaticParams() {
+  const problems = await getDocs(collection(firestore, "problems"));
+  return problems.docs.map((d) => ({ slug: d.data()["id"] }));
+}
 
 
 
