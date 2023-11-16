41a758717c951df705daf8d5af8cf440feb9702c
TanPhat23 <ptp112004@gmail.com>
2023-11-16 16:23:23 +0700

    fix error

--- my-app/components/ErrorBoundary.tsx
+++ my-app/components/ErrorBoundary.tsx
@@ -4,19 +4,35 @@ type ErrorBoundaryProps = {
   fallback: ReactNode;
   children: ReactNode;
 };
-class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
-  state = { hasError: false };
-  static getDerivedStateFromError(error: any) {
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: any): ErrorBoundaryState {
     return { hasError: true };
   }
+
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
     console.log(error, errorInfo);
+    // You can also log the error to a service or perform other actions
   }
+
   render(): React.ReactNode {
     if (this.state.hasError) {
-      return this.props.fallback;
+      return this.props.fallback; // Display the fallback UI when an error occurs
     }
-    return this.props.fallback;
+    return this.props.children; // Render the children components as usual
   }
 }
+
 export default ErrorBoundary;
