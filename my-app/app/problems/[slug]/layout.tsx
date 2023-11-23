47d2644b4220b4f0b50f200c6e5cfaa6d10a30f7
TanPhat23 <ptp112004@gmail.com>
2023-11-23 17:36:50 +0700

    moved generateStaticParams to layout

--- my-app/app/problems/[slug]/layout.tsx
+++ my-app/app/problems/[slug]/layout.tsx
@@ -1,9 +1,14 @@
+import { firestore } from "@/lib/firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
+export async function generateStaticParams() {
+  const problems = await getDocs(collection(firestore, "problems"));
+  return problems.docs.map((d) => ({ slug: d.data()["id"] }));
+}
+
 const PageLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
     </div>
   );
 };
--- my-app/app/problems/[slug]/page.tsx
+++ my-app/app/problems/[slug]/page.tsx
@@ -3,13 +3,7 @@ import Workspace from "@/components/problemComponents/Workspace";
 
 import { problems } from "@/lib/problems";
 import { notFound } from "next/navigation";
-import { firestore } from "@/lib/firebase/firebase";
-import { collection, getDocs } from "firebase/firestore";
 
-export async function generateStaticParams() {
-  const problems = await getDocs(collection(firestore, "problems"));
-  return problems.docs.map((d) => ({ slug: d.data()["id"] }));
-}
 
 export default async function ProblemPage({
   params,
